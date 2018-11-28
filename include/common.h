a4cd89d4f3edda144356018f36cd046ede10ec84
Faisal Alghamdi <falgh170@gmail.com>
2018-11-28 12:11:20 +0300

    4

--- include/common.h
+++ include/common.h
@@ -100,9 +100,9 @@ typedef volatile unsigned char	vu_char;
 #define CONFIG_SYS_SUPPORT_64BIT_DATA
 #endif
 
-#ifndef DEBUG
-#define DEBUG
-#endif
+//#ifndef DEBUG
+//#define DEBUG
+//#endif
 
 
 #ifdef DEBUG
